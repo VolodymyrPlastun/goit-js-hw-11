6a3dcd8c8675b261380280b35ecc6f020673462f
VolodymyrPlastun <vplastun8@gmail.com>
2022-04-13 11:47:51 +0300

    in process

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import ImagesApiServise from './fetchImages-api';
-import imagesMarcup from './gallery.hbs';
+// import imagesMarcup from './gallery.hbs';
 
 const imagesApi = new ImagesApiServise();
 const refs = {
@@ -20,41 +20,43 @@ function OnSubmitSearch (evt) {
 
     imagesApi.value = evt.currentTarget.elements.searchQuery.value;
   imagesApi.resetPage();
-  imagesApi.fetchImages().then(createGalleryMarckup);
+  // imagesApi.fetchImages().then(createGalleryMarckup);
+  imagesApi.fetchImages().then(searchImages);
 }
 
 function onLoadMore() {
- imagesApi.fetchImages().then(createGalleryMarckup);
+//  imagesApi.fetchImages().then(createGalleryMarckup);
+  imagesApi.fetchImages().then(searchImages);
 }
 
-function createGalleryMarckup(images) {
-  refs.gallery.insertAdjacentHTML('beforeend', imagesMarcup(images));
-}
-
-// function searchImages(images) {
-//     // console.log(images.data.hits);
-//      const galleryMarcup = images.data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-//          return `<div class="photo-card">
-//   <a class="class="gallery__link"" href="${largeImageURL}">
-//     <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-//     <div class="info">
-//       <p class="info-item">
-//         <b>${likes}</b>
-//       </p>
-//       <p class="info-item">
-//         <b>${views}</b>
-//       </p>
-//       <p class="info-item">
-//         <b>${comments}</b>
-//       </p>
-//       <p class="info-item">
-//         <b>${downloads}</b>
-//       </p>
-//     </div>
-//   </a>
-// </div>`
-//      }).join('');
-//     refs.gallery.insertAdjacentHTML('beforeend', galleryMarcup);
+// function createGalleryMarckup(images) {
+//   refs.gallery.insertAdjacentHTML('beforeend', imagesMarcup(images));
 // }
+
+function searchImages(images) {
+    // console.log(images);
+     const galleryMarcup = images.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+         return `<div class="photo-card">
+  <a class="gallery__link" href="${largeImageURL}">
+    <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>${views}</b>
+      </p>
+      <p class="info-item">
+        <b>${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>${downloads}</b>
+      </p>
+    </div>
+  </a>
+</div>`
+     }).join('');
+    refs.gallery.insertAdjacentHTML('beforeend', galleryMarcup);
+}
 
 // let gallery = new SimpleLightbox('.gallery a');
